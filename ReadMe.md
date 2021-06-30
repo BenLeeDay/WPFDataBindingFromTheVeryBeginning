2b1a145209a6d5afbc4a44bb70b7d612182038e5
BenLeeDay <64696963+bld96@users.noreply.github.com>
2021-06-30 08:16:50 +0100

    Add project files.

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# WPF data binding from the very beginning
+## Overview
+This repo contains the source code associated with my tutorial series on WPF data binding for beginners.
+
+You can access part 1 of the series here: wwww.BenLeeDay.com/
+
+## Tags
+The repo is tagged in accordance with the articles on BenLeeDay.com: the source code associated with the first article has the tag 1.0. The first digit will increment by 1 each time a new article is posted. If an article has multiple source codes, the second digit will increment.
+
+As new articles are posted, more tags will be added. A list of the tags for this repo can be found here: https://github.com/BenLeeDay/WPFDataBindingFromTheVeryBeginning/tags
---
+++ ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+
+namespace DataBindingExercise
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+        private string _exampleString;
+        #endregion
+
+        #region Constructor
+        public ViewModel()
+        {
+            ExampleString = "Here is some text which is loaded at startup.";
+        }
+        #endregion
+
+        #region Properties
+        public string ExampleString
+        {
+            get { return _exampleString; }
+            set
+            {
+                _exampleString = value;
+                OnPropertyChanged("ExampleString");
+            }
+        }
+        #endregion
+
+        #region INotifyPropertyChanged members
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+        #endregion
+    }
+}
