48c84afd1ebb2bcf24b971609fa80590aeef2a90
BenLeeDay <64696963+bld96@users.noreply.github.com>
2021-07-06 07:59:59 +0100

    Simplify ViewModel

--- ViewModel.cs
+++ ViewModel.cs
@@ -2,26 +2,11 @@
 {
     public class ViewModel
     {
-        #region Fields
-        private string _exampleString;
-        #endregion
-
-        #region Constructor
         public ViewModel()
         {
             ExampleString = "Here is some text which is loaded at startup.";
         }
-        #endregion
 
-        #region Properties
-        public string ExampleString
-        {
-            get { return _exampleString; }
-            set
-            {
-                _exampleString = value;
-            }
-        }
-        #endregion
+        public string ExampleString { get; set; }
     }
 }
