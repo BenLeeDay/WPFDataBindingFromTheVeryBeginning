2473fdac4565fb23a6712f0f46c1f4ea0b103261
BenLeeDay <64696963+bld96@users.noreply.github.com>
2021-07-06 06:51:29 +0100

    Remove INotifyPropertyChanged

--- ViewModel.cs
+++ ViewModel.cs
@@ -1,39 +1,11 @@
-﻿using System.ComponentModel;
-
-namespace DataBindingExercise
+﻿namespace DataBindingExercise
 {
-    public class ViewModel : INotifyPropertyChanged
+    public class ViewModel
     {
-        #region Fields
-        private string _exampleString;
-        #endregion
-
-        #region Constructor
         public ViewModel()
         {
             ExampleString = "Here is some text which is loaded at startup.";
         }
-        #endregion
-
-        #region Properties
-        public string ExampleString
-        {
-            get { return _exampleString; }
-            set
-            {
-                _exampleString = value;
-                OnPropertyChanged("ExampleString");
-            }
-        }
-        #endregion
-
-        #region INotifyPropertyChanged members
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string v)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
-        }
-        #endregion
+        public string ExampleString { get; set; }
     }
 }
